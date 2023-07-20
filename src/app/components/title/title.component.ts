f0f3ce9fe8341025d505021b54b2c58b9492ac07
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2023-07-19 22:46:21 -0300

    add: first commit

---
+++ src/app/components/title/title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss','./title.responsive.component.scss']
+})
+export class TitleComponent {
+
+}
